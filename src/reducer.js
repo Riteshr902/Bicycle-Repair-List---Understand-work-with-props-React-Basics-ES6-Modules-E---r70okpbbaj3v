394d8e303de7ea73fd064cf18f32fffabfc61150
Ritesh Kumar <110487386+Riteshr902@users.noreply.github.com>
2023-02-24 11:56:18 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,4 @@
-/*
-reducer is defined here and is exproted.
-This reducer should handle all the actions.
-Example of how to use reducer is as follows:
-const reducer = (state = [],action = {}) => {
-  switch(action.type){
-    case 'actionType1': 
-      return changedState1;
-    
-    case 'actionType2':
-      return changedState2;
-    default: 
-      return state;
-    }
-  
-export default reducer;
-*/
+
 /*
 reducer is defined here and is exproted.
 This reducer should handle all the actions.
@@ -101,4 +85,3 @@ const reducers = combineReducers({
 });
 
 export default reducers;
-Footer
